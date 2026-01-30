4bb13b1fde4373d1bed939909ad7283fcf9fcdac
glorycornel <chiomaowanku@gmail.com>
2026-01-30 17:02:09 +0100

    Fix nodemailer import for CommonJS

--- apps/api/src/modules/auth/mail.service.ts
+++ apps/api/src/modules/auth/mail.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import nodemailer from 'nodemailer';
+import * as nodemailer from 'nodemailer';
 
 type MailMessage = {
   to: string;
